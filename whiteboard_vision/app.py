c8f100033198607551c0e9734f02a9f2a7afe9df
Brikwerk <reece@brikwerk.com>
2019-10-24 02:32:31 -0700

    Added bbox image cropping and output

--- whiteboard_vision/app.py
+++ whiteboard_vision/app.py
@@ -14,10 +14,12 @@ import numpy as np
 from PIL import Image
 
 from .clova_detection.api import detect_text
+from .clova_recognition.api import recognize_text
+from . import imgutil
 
 def process_images(images_path):
     # Getting all images with an acceptable extension under the given path
-    image_extensions = [".png", ".jpg", ".jpeg", ".tiff"]
+    image_extensions = [".png", ".jpg", ".jpeg", ".tif"]
     files = [os.path.join(images_path, file_name) for file_name in os.listdir(images_path)
                 if any(file_name.endswith(extension) for extension in images_path)]
     if len(files) == 0:
@@ -28,12 +30,13 @@ def process_images(images_path):
         boxes, polys = detect_text(image_path)
         image = cv2.imread(image_path)
 
+        count = 0
         for box in boxes:
-            print(box)
-            print(box[0][0], box[1][0], box[0][1], box[2][1])
-            cropped_image = image[int(box[0][0]):int(box[1][0]), int(box[0][0]):int(box[2][1])]
-            cv2.imshow("Image", cropped_image)
-            cv2.waitKey(0)
-            break
+            bbox_image = imgutil.crop_rectangle(box, image)
+            bbox_image_path = "./bbox_results/%s_%s.jpg" % (os.path.basename(image_path), count)
+            count = count + 1
+            if not os.path.exists("./bbox_results"):
+                os.makedirs("./bbox_results")
+            cv2.imwrite(bbox_image_path, bbox_image)
     
     return images_path
