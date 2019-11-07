0e9fcc4f1711bca0a8c017fd1c9c1e60ef86e714
Brikwerk <reece@brikwerk.com>
2019-11-06 22:28:35 -0800

    Added debug option

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ import whiteboard_vision as wv
 
 def main():
     demo_images_path = os.path.abspath("test_images")
-    results = wv.process_images(demo_images_path)
+    results = wv.process_images(demo_images_path, debug=True)
     print(results)
 
 if __name__ == '__main__':
