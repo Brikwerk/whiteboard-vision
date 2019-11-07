fb4b90bf01d96edb69a82270225eeb6d27772651
Brikwerk <reece@brikwerk.com>
2019-11-06 22:33:03 -0800

    Changed recognition to object instantiation

--- whiteboard_vision/clova_recognition/api.py
+++ whiteboard_vision/clova_recognition/api.py
@@ -6,6 +6,7 @@ import os
 import torch
 import torch.backends.cudnn as cudnn
 import torch.utils.data
+import torch.nn.functional as F
 
 from .utils import CTCLabelConverter, AttnLabelConverter
 from .dataset import RawDataset, AlignCollateNoLabels, ImageDataset
@@ -18,7 +19,7 @@ def recognize_text(images,
     batch_size=192, 
     saved_model=None,
     batch_max_length=25,
-    imgH=32, imgW=100, rgb=True,
+    imgH=32, imgW=100, rgb=False,
     character='0123456789abcdefghijklmnopqrstuvwxyz',
     sensitive=False,
     PAD=False,
@@ -82,34 +83,6 @@ def recognize_text(images,
     }
     opt = SimpleNamespace(**opt_json)
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--images', help='path to image_folder which contains text images')
-    parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
-    parser.add_argument('--batch_size', type=int, default=192, help='input batch size')
-    parser.add_argument('--saved_model', help="path to saved_model to evaluation")
-    """ Data processing """
-    parser.add_argument('--batch_max_length', type=int, default=25, help='maximum-label-length')
-    parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
-    parser.add_argument('--imgW', type=int, default=100, help='the width of the input image')
-    parser.add_argument('--rgb', action='store_true', help='use rgb input', default=False)
-    parser.add_argument('--character', type=str, default='0123456789abcdefghijklmnopqrstuvwxyz', help='character label')
-    parser.add_argument('--sensitive', action='store_true', help='for sensitive character mode')
-    parser.add_argument('--PAD', action='store_true', help='whether to keep ratio then pad for image resize')
-    """ Model Architecture """
-    parser.add_argument('--Transformation', type=str, default="TPS", help='Transformation stage. None|TPS')
-    parser.add_argument('--FeatureExtraction', type=str, default="ResNet", help='FeatureExtraction stage. VGG|RCNN|ResNet')
-    parser.add_argument('--SequenceModeling', type=str, default="BiLSTM", help='SequenceModeling stage. None|BiLSTM')
-    parser.add_argument('--Prediction', type=str, default="Attn", help='Prediction stage. CTC|Attn')
-    parser.add_argument('--num_fiducial', type=int, default=20, help='number of fiducial points of TPS-STN')
-    parser.add_argument('--input_channel', type=int, default=1, help='the number of input channel of Feature extractor')
-    parser.add_argument('--output_channel', type=int, default=512,
-                        help='the number of output channel of Feature extractor')
-    parser.add_argument('--hidden_size', type=int, default=256, help='the size of the LSTM hidden state')
-
-    opt = parser.parse_args()
-    opt.saved_model = saved_model
-    opt.images = images
-
     if 'CTC' in opt.Prediction:
         converter = CTCLabelConverter(opt.character)
     else:
@@ -166,10 +139,132 @@ def recognize_text(images,
                 preds_str = converter.decode(preds_index, length_for_pred)
 
             print('-' * 80)
-            print('image_path\tpredicted_labels')
+            print('predicted_label\tConfidence Score')
             print('-' * 80)
-            for pred in preds_str:
+            preds_prob = F.softmax(preds, dim=2)
+            preds_max_prob, _ = preds_prob.max(dim=2)
+            for pred, pred_max_prob in zip(preds_str, preds_max_prob):
                 if 'Attn' in opt.Prediction:
-                    pred = pred[:pred.find('[s]')]  # prune after "end of sentence" token ([s])
+                    pred_EOS = pred.find('[s]')
+                    pred = pred[:pred_EOS]  # prune after "end of sentence" token ([s])
+                    pred_max_prob = pred_max_prob[:pred_EOS]
 
-                print(f'{pred}')
+                # calculate confidence score (= multiply of pred_max_prob)
+                confidence_score = pred_max_prob.cumprod(dim=0)[-1]
+
+                print(f'{pred}\t{confidence_score}')
+
+class CraftRecognition:
+    def __init__(self, workers=4, 
+    batch_size=192, 
+    saved_model=None,
+    batch_max_length=25,
+    imgH=32, imgW=100, rgb=False,
+    character='0123456789abcdefghijklmnopqrstuvwxyz',
+    sensitive=False,
+    PAD=False,
+    Transformation="TPS",
+    FeatureExtraction="ResNet",
+    SequenceModeling="BiLSTM",
+    Prediction="Attn",
+    num_fiducial=20,
+    input_channel=1, output_channel=512,
+    hidden_size=256):
+        torch.multiprocessing.freeze_support()
+        if saved_model == None:
+            # Getting model path
+            saved_model = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                "weights/TPS-ResNet-BiLSTM-Attn.pth"))
+
+        # Assembling options
+        opt_json = {
+            "workers": workers,
+            "batch_size": batch_size,
+            "saved_model": saved_model,
+            "batch_max_length": batch_max_length,
+            "imgH": imgH,
+            "imgW": imgW,
+            "rgb": rgb,
+            "character": character,
+            "sensitive": sensitive,
+            "PAD": PAD,
+            "Transformation": Transformation,
+            "FeatureExtraction": FeatureExtraction,
+            "SequenceModeling": SequenceModeling,
+            "Prediction": Prediction,
+            "num_fiducial": num_fiducial,
+            "input_channel": input_channel,
+            "output_channel": output_channel,
+            "hidden_size": hidden_size
+        }
+        self.opt = SimpleNamespace(**opt_json)
+
+        if 'CTC' in self.opt.Prediction:
+            self.converter = CTCLabelConverter(self.opt.character)
+        else:
+            self.converter = AttnLabelConverter(self.opt.character)
+        self.opt.num_class = len(self.converter.character)
+
+        if self.opt.rgb:
+            self.opt.input_channel = 3
+        self.model = Model(self.opt)
+        self.model = torch.nn.DataParallel(self.model).to(device)
+
+        # load model
+        print('Loading Craft Recognition Model from %s' % self.opt.saved_model)
+        if torch.cuda.is_available():
+            self.model.load_state_dict(torch.load(self.opt.saved_model))
+        else:
+            self.model.load_state_dict(torch.load(self.opt.saved_model, map_location=torch.device('cpu')))
+        
+        self.model.eval()
+    
+
+    def recognize_text(self, images):
+        AlignCollate_images = AlignCollateNoLabels(imgH=self.opt.imgH, imgW=self.opt.imgW, keep_ratio_with_pad=self.opt.PAD)
+        image_data = ImageDataset(images, self.opt.rgb)
+        image_loader = torch.utils.data.DataLoader(
+            image_data, batch_size=self.opt.batch_size,
+            shuffle=False,
+            num_workers=int(self.opt.workers),
+            collate_fn=AlignCollate_images, pin_memory=True)
+
+        with torch.no_grad():
+            for image_tensors in image_loader:
+                batch_size = image_tensors.size(0)
+                image = image_tensors.to(device)
+                # For max length prediction
+                length_for_pred = torch.IntTensor([self.opt.batch_max_length] * batch_size).to(device)
+                text_for_pred = torch.LongTensor(batch_size, self.opt.batch_max_length + 1).fill_(0).to(device)
+
+                if 'CTC' in self.opt.Prediction:
+                    preds = self.model(image, text_for_pred).log_softmax(2)
+
+                    # Select max probabilty (greedy decoding) then decode index to character
+                    preds_size = torch.IntTensor([preds.size(1)] * batch_size)
+                    _, preds_index = preds.permute(1, 0, 2).max(2)
+                    preds_index = preds_index.transpose(1, 0).contiguous().view(-1)
+                    preds_str = self.converter.decode(preds_index.data, preds_size.data)
+
+                else:
+                    preds = self.model(image, text_for_pred, is_train=False)
+
+                    # select max probabilty (greedy decoding) then decode index to character
+                    _, preds_index = preds.max(2)
+                    preds_str = self.converter.decode(preds_index, length_for_pred)
+
+                preds_prob = F.softmax(preds, dim=2)
+                preds_max_prob, _ = preds_prob.max(dim=2)
+                results = []
+                for pred, pred_max_prob in zip(preds_str, preds_max_prob):
+                    if 'Attn' in self.opt.Prediction:
+                        pred_EOS = pred.find('[s]')
+                        pred = pred[:pred_EOS]  # prune after "end of sentence" token ([s])
+                        pred_max_prob = pred_max_prob[:pred_EOS]
+
+                    # calculate confidence score (= multiply of pred_max_prob)
+                    confidence_score = pred_max_prob.cumprod(dim=0)[-1]
+
+                    results.append([pred, confidence_score])
+                
+                return results
