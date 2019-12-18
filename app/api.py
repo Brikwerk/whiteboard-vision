36c3483ffd486fca1a2265305da1c3fee9d76dec
Brikwerk <reece@brikwerk.com>
2019-12-17 22:10:10 -0800

    Added error handling for recognition

--- app/api.py
+++ app/api.py
@@ -7,29 +7,35 @@ import os
 
 @app.route("/api/v1/imagerecognition", methods=["POST"])
 def image_recognition():
-    # Saving files to a directory
-    image_dict = request.files.to_dict()
-    image_paths = []
-    for image_key in image_dict:
-        f = image_dict[image_key]
-        filename = secure_filename(image_key)
+    try:
+        # Saving files to a directory
+        image_dict = request.files.to_dict()
+        image_paths = []
+        for image_key in image_dict:
+            f = image_dict[image_key]
+            filename = secure_filename(image_key)
 
-        # Checking if uploads directory is available
-        # If not, creating
-        if not os.path.exists("./uploads"):
-            os.makedirs("./uploads")
-        
-        # Saving image with secure filename
-        image_path = os.path.join("./uploads", filename)
-        image_paths.append(image_path)
-        f.save(image_path)
+            # Checking if uploads directory is available
+            # If not, creating
+            if not os.path.exists("./uploads"):
+                os.makedirs("./uploads")
+            
+            # Saving image with secure filename
+            image_path = os.path.join("./uploads", filename)
+            image_paths.append(image_path)
+            f.save(image_path)
+    except:
+        return {"error": "An error was encountered uploading and saving your image"}
     
-    # Loading files from directory and performing recognition
-    images = load_image_paths(image_paths)
-    results = process_images(images)
+    try:
+        # Loading files from directory and performing recognition
+        images = load_image_paths(image_paths)
+        results = process_images(images)
 
-    # Deleting uploaded images
-    for path in image_paths:
-        os.remove(path)
+        # Deleting uploaded images
+        for path in image_paths:
+            os.remove(path)
+    except:
+        return {"error": "An error was encountered processing your images"}
 
     return results
