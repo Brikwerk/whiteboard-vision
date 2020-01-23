0914e7b9cde2d0fb6b3c5e3b21c12a693619fd6a
Brikwerk <reece@brikwerk.com>
2020-01-22 16:45:19 -0800

    Added image upload improvements

--- app/static/js/wv-image.js
+++ app/static/js/wv-image.js
@@ -8,6 +8,26 @@ function showPhotoUploadSection() {
 }
 
 
+function CreateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+function createButton(buttonText, buttonType) {
+    let button = document.createElement("button");
+    button.classList.add("uk-button", "uk-button-primary");
+    button.innerHTML = buttonText;
+
+    if (buttonType === "large") {
+        button.classList.add("uk-width-1-1", "uk-margin-small-bottom");
+    }
+
+    return button
+}
+
+
 function renderUploadedImages(imageFiles) {
     if (imageFiles.length == 0) {
         return;
@@ -33,7 +53,7 @@ function renderUploadedImages(imageFiles) {
         reader.onload = function (evt) {
             container = document.getElementById("uploads-container");
 
-            imageContainer = renderImageContainer(evt.target.result);
+            imageContainer = renderImageContainer(evt.target.result, name);
 
             container.appendChild(imageContainer);
         };
@@ -45,27 +65,45 @@ function renderUploadedImages(imageFiles) {
 }
 
 
-function renderImageContainer(imageBase64) {
+function renderImageContainer(imageBase64, containerName) {
     let imageContainer = document.createElement("div");
+    imageContainer.id = containerName + "-container";
+    imageContainer.setAttribute("imageName", containerName);
+    imageContainer.classList.add("image-upload-container");
 
     // Making tab section
     let tabContainer = document.createElement("ul");
     let firstTab = document.createElement("li");
-    firstTab.innerHTML = "Original";
+
+    firstTab.innerHTML = "<a href='#'>Original</a>";
     firstTab.classList.add("uk-active");
+
     tabContainer.appendChild(firstTab);
-    UIkit.tab(tabContainer);
+    tabContainer.classList.add("uk-tab", "image-tabs");
 
     // Making switch section
     let switcherContainer = document.createElement("ul")
     let firstSwitcher = document.createElement("li");
     let image = document.createElement("img");
+
     image.setAttribute("src", imageBase64);
+
     firstSwitcher.appendChild(image);
+    firstSwitcher.classList.add("uk-active");
+
+    // Making & Adding buttons to first switcher for ROI selection
+    let buttonContainer = document.createElement("p")
+    buttonContainer.classList.add("uk-margin");
+
+    let clearButton = createButton("Clear Selection", "large");
+    let detectButton = createButton("Detect Text in Selection", "large");
+    buttonContainer.appendChild(clearButton);
+    buttonContainer.appendChild(detectButton);
+
+    firstSwitcher.appendChild(buttonContainer);
+
     switcherContainer.appendChild(firstSwitcher);
-    switcherContainer.classList.add("uk-switcher")
-    switcherContainer.classList.add("uk-margin")
-    UIkit.switcher(switcherContainer);
+    switcherContainer.classList.add("uk-switcher", "image-switcher");
 
     // Appending tabs & switcher to container
     imageContainer.appendChild(tabContainer);
